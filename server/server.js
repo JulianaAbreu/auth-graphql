c16a4be98315a304e967268107a094b117b5143e
JulianaAbreu <juliana.cerqueira55@gmail.com>
2018-05-02 16:50:29 -0300

    first commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:admin@ds251849.mlab.com:51849/lyricaldb';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
